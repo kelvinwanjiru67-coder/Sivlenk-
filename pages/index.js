c0c73532f450569f43f5c10fd2ceb54ff6d3d4ff
kelvinwanjiru67-coder <kelvinwanjiru67@gmail.com>
2026-01-10 19:10:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const products = [
     desc: "A proven digital system designed to help you build scalable online income streams using modern strategies.",                       
     link: "https://www.claudiacaldwell.com/oto-uf61a?el=splittest-1214-bradflow-control#aff=Kelvis67",
     type: "Digital",
-    image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f"
+    image: "https://unsplash.com/photos/silver-fork-and-bread-knife-W2QVimqre4w"
   },
 
   {
