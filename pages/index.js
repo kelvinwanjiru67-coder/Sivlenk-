167e5bc631e199448c840c8ac5f53f756c171ee5
kelvinwanjiru67-coder <kelvinwanjiru67@gmail.com>
2026-01-11 04:41:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,24 +47,15 @@ export default function Home() {
       </Head>
 
       <main
-  style={{
-    minHeight: "100vh",
-    width: "100%",
-    background: "#020617",
-    color: "#e5e7eb",
-    padding: "1rem",
-    boxSizing: "border-box"
-  }}
-<div
-  style={{
-    maxWidth: "1200px",
-    margin: "0 auto",
-    width: "100%"
-  }}
->
-    >
-  </div>
-</main>
+        style={{
+          minHeight: "100vh",
+          width: "100%",
+          background: "#020617",
+          color: "#e5e7eb",
+          padding: "1rem",
+          boxSizing: "border-box"
+       }}
+     >
 
 
 
