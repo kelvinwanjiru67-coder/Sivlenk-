082303b84c5d9455fb577abe2d8528934373d8e4
kelvinwanjiru67-coder <kelvinwanjiru67@gmail.com>
2026-01-14 10:27:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -87,6 +87,7 @@ export default function Home() {
     }}
   >
     {[
+      { name: "Home", link: "/" },
       { name: "Dating", link: "/dating" },
       { name: "Wellness", link: "/wellness" },
       { name: "Beauty", link: "/beauty" },
